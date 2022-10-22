aeca3af0d735e03623ba5e0912cf61f7f81c1e6b
MuGuiLin <muguilin@foxmail.com>
2022-10-22 11:52:56 +0800

    update

--- CDN-Vue/js/components/mu-input.js
+++ CDN-Vue/js/components/mu-input.js
@@ -1,24 +1,43 @@
-import { defineComponent } from '../package/vue.esm-browser.js';
+import { defineComponent, ref } from '../package/vue.esm-browser.js';
 
 export default defineComponent({
     name: 'mu-input',
-    template: `<input type="text" v-model="value" @input="input" />`,
+    template: `
+    <label>{{label}}：<input :type="type" v-model="value" @input="input" /></label>
+    <label>{{label}}：<input :type="type" v-model="value" @input="input2" /></label>
+    `,
     props: {
+        label: {
+            type: String,
+            default: '标题'
+        },
+        type: String,
         // value: {
         //     type: Number | String,
         //     default: 0
         // }
-        // value: Number | String,
+        value: Number | String,
+        input: Function
     },
     setup(props, ctx) {
-        console.log(1, props, ctx)
+        console.log(1, props, ctx);
 
-        const input = (e) => {
-            console.log(e);
-            // ctx.emit('input', e);
-        }
-        return{
-            input
-        }
+        const num = ref(0);
+
+        const input = ({ target }) => {
+            console.log(target.value);
+            ctx.emit('input', target.value);
+        };
+
+        const input2 = ({ target }) => {
+            console.log(target.value);
+            props.input(target.value)
+        };
+
+        return {
+            num,
+            input,
+            input2
+        };
     }
 });
